218258e8e6c40d95e39f06e93638892d2fa39aa9
TheKingsident <kingsident@gmail.com>
2024-02-11 20:44:51 +0100

    Added and updated some files

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+    let checkedAmenities = {};
+
+    $('input[type="checkbox"]').change(function() {
+        if (this.checked) {
+            checkedAmenities[$(this).data('id')] = $(this).data('name');
+        } else {
+            delete checkedAmenities[$(this).data('id')];
+        }
+        $('.amenities h4').text(Object.values(checkedAmenities).join(', '));
+    });
+
+    // Check API status
+    $.get('http://localhost:5001/api/v1/status/', function(data) {
+        console.log(data);
+        if (data.status === "OK") {
+            $('#api_status').addClass('available');
+        } else {
+            $('#api_status').removeClass('available');
+        }
+    }).fail(function() {
+        $('#api_status').removeClass('available');
+    });
+});
