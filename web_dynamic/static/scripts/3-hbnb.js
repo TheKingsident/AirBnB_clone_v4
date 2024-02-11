10a30cdde1e699d994de135ec48a1143de38d818
TheKingsident <kingsident@gmail.com>
2024-02-12 00:00:43 +0100

    Modified some files

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
         $('.amenities h4').text(Object.values(checkedAmenities).join(', '));
     });
 
-    $.get('http://0.0.0.0:5001/api/v1/status/', function(data) {
+    $.get('http://localhost:5001/api/v1/status/', function(data) {
         console.log(data);
         if (data.status === "OK") {
             $('#api_status').addClass('available');
@@ -22,28 +22,40 @@ $(document).ready(function () {
     });
 
     $.ajax({
-        url: 'http://0.0.0.0:5001/api/v1/places_search/',
+        url: 'http://localhost:5001/api/v1/places_search/',
         type: 'POST',
         contentType: 'application/json',
         data: JSON.stringify({}),
-        success: function(data) {
-            const placesContainer = $('.places');
-            placesContainer.empty();
-            data.forEach(function(place) {
-                const placeArticle = $('<article></article>');
-                placeArticle.append(`<h2>${place.name}</h2>`);
-                placeArticle.append(`<div class="price_by_night">$${place.price_by_night}</div>`);
-                placeArticle.append(`<div class="information">`);
-                placeArticle.append(`<div class="max_guest">${place.max_guest} Guest${place.max_guest !== 1 ? 's' : ''}</div>`);
-                placeArticle.append(`<div class="number_rooms">${place.number_rooms} Bedroom${place.number_rooms !== 1 ? 's' : ''}</div>`);
-                placeArticle.append(`<div class="number_bathrooms">${place.number_bathrooms} Bathroom${place.number_bathrooms !== 1 ? 's' : ''}</div>`);
-                placeArticle.append(`</div>`);
-                placeArticle.append(`<div class="description">${place.description}</div>`);
-                placesContainer.append(placeArticle);
-            });
+        success: function (data) {
+          const placesContainer = $('.places');
+          placesContainer.empty();
+          data.forEach(function (place) {
+            const placeArticle = $('<article></article>');
+      
+            // Title box
+            const titleBoxDiv = $('<div class="title_box"></div>');
+            titleBoxDiv.append(`<h2>${place.name}</h2>`);
+            titleBoxDiv.append(`<div class="price_by_night">$${place.price_by_night}</div>`);
+            placeArticle.append(titleBoxDiv);
+      
+            // Information box
+            const infoDiv = $('<div class="information"></div>');
+            infoDiv.append(`<div class="max_guest">${place.max_guest} Guest${place.max_guest !== 1 ? 's' : ''}</div>`);
+            infoDiv.append(`<div class="number_rooms">${place.number_rooms} Bedroom${place.number_rooms !== 1 ? 's' : ''}</div>`);
+            infoDiv.append(`<div class="number_bathrooms">${place.number_bathrooms} Bathroom${place.number_bathrooms !== 1 ? 's' : ''}</div>`);
+            placeArticle.append(infoDiv);
+      
+            // Description box
+            const descriptionDiv = $('<div class="description"></div>');
+            descriptionDiv.html(place.description);
+            placeArticle.append(descriptionDiv);
+      
+            placesContainer.append(placeArticle);
+          });
         }
-    }).fail(function() {
+      }).fail(function () {
         console.log("Failed to retrieve places.");
-    });
+      });
+      
 });
 
